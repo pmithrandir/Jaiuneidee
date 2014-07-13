0ebe9409518a2cc59550a2b57eaca137bb840842
Pierre Bonneau <pmithrandir@users.noreply.github.com>
2014-07-13 16:58:07 +0000

    remove unload event in js + add votes in ajax

--- src/JaiUneIdee/SiteBundle/Controller/VoteController.php
+++ src/JaiUneIdee/SiteBundle/Controller/VoteController.php
@@ -16,37 +16,37 @@ class VoteController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         if (true === $this->get('security.context')->isGranted('ROLE_USER')) {
-	        $user = $this->get('security.context')->getToken()->getUser();
-	        $idee = $em->getRepository('JaiUneIdeeSiteBundle:Idee')->find($idee_id);
-	        if (!$idee) {
-	            throw $this->createNotFoundException('Unable to find Idee.');
-	        }
-		$voteExistant = $em->getRepository('JaiUneIdeeSiteBundle:Vote')->getVotesByIdeeAndUser($idee, $user);
-	    	if(!$voteExistant ){
-		    	$vote = new Vote();
-		    	$vote->setIdee($idee);
-		    	$vote->setUser($user);
-		    	switch ($note){
-		    		case 1:
-		    		case -1:
-		    		case 0:
-		    			$vote->setNote($note);
-		    		break;
-		    		default:
-		    			throw $this->createNotFoundException('Invalid Value.');
-		    		break;
-		    	}
-		    	
-		        $em->persist($vote);
-		        $em->flush();
-	    	}
-	    	else{
-	    		throw $this->createNotFoundException('Vous avez déjà voté pour cette idée.');
-	    	}
-	        return $this->redirect($this->generateUrl('JaiUneIdeeSiteBundle_idee_show', array(
-	            'id' => $idee_id ,
-	            'slug' => $vote->getIdee()->getSlug()))
-	        );
+            $user = $this->get('security.context')->getToken()->getUser();
+            $idee = $em->getRepository('JaiUneIdeeSiteBundle:Idee')->find($idee_id);
+            if (!$idee) {
+                throw $this->createNotFoundException('Unable to find Idee.');
+            }
+            $voteExistant = $em->getRepository('JaiUneIdeeSiteBundle:Vote')->getVotesByIdeeAndUser($idee, $user);
+            if (!$voteExistant) {
+                $vote = new Vote();
+                $vote->setIdee($idee);
+                $vote->setUser($user);
+                switch ($note) {
+                    case 1:
+                    case -1:
+                    case 0:
+                        $vote->setNote($note);
+                        break;
+                    default:
+                        throw $this->createNotFoundException('Invalid Value.');
+                        break;
+                }
+
+                $em->persist($vote);
+                $em->flush();
+            } else {
+                throw $this->createNotFoundException('Vous avez déjà voté pour cette idée.');
+            }
+            $url = $this->getRequest()->isXMLHttpRequest() ? 'JaiUneIdeeSiteBundle_idee_votes' : 'JaiUneIdeeSiteBundle_idee_show';
+            return $this->redirect($this->generateUrl($url, array(
+                                'id' => $idee_id,
+                                'slug' => $idee->getSlug()))
+            );
         }
     }
     
@@ -65,10 +65,12 @@ class VoteController extends Controller
                 $em->persist($voteExistant);
                 $em->flush();
             }
-            return $this->redirect($this->generateUrl('JaiUneIdeeSiteBundle_idee_show', array(
-            'id' => $idee_id ,
-            'slug' => $idee->getSlug()))
-        );
+            
+            $url = $this->getRequest()->isXMLHttpRequest() ? 'JaiUneIdeeSiteBundle_idee_votes' : 'JaiUneIdeeSiteBundle_idee_show';
+            return $this->redirect($this->generateUrl($url, array(
+                                'id' => $idee_id,
+                                'slug' => $idee->getSlug()))
+            );
         }
        
     }
