9fc23189c8840d3a93cea6a2b32dc365637eaf23
pierre <pierre@db0731ed-5a52-4649-9917-173120e95d3f>
2015-02-21 12:25:47 +0000

    last action date

---
+++ trunk/app/DoctrineMigrations/Version20150221101235.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150221101235 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        
+        $this->addSql('ALTER TABLE idee ADD last_action_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        
+        $this->addSql('ALTER TABLE idee DROP last_action_at');
+    }
+}
